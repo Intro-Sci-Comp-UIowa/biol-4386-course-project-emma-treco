bb35f4a3f32c9c3cb1d98982c79b0d6dcc957304
emmatreco <emma-treco@uiowa.edu>
2023-04-05 15:03:29 -0500

    draft of female mouse data analysis

--- analysis/TRAF3_Project_code.R
+++ analysis/TRAF3_Project_code.R
@@ -1,5 +1,25 @@
-library(readxl)
-ALL_T3_1Ms_Bcell_IL6_TC <- read_excel("Desktop/ALL_T3_1Ms_Bcell_IL6_TC.xlsx")
-View(ALL_T3_1Ms_Bcell_IL6_TC)
+# open and view data
+  library(readxl)
+  library(tidyverse)
+  ALL_T3_1Ms_Bcell_IL6_TC <- read_excel("Desktop/ALL_T3_1Ms_Bcell_IL6_TC.xlsx")
+  ALL_T3_1Ms_Bcell_IL6_TC$`pSTAT3Y705/STAT3` <- as.numeric(as.character(ALL_T3_1Ms_Bcell_IL6_TC$`pSTAT3Y705/STAT3`))
 
-
+#First graph will be of just the female mice, so I will start by filtering out the males.
+  Female_T3_1Ms_Bcell_IL6_TC <- ALL_T3_1Ms_Bcell_IL6_TC %>% filter(Sex=="Female") %>% 
+    select("Genotype", "Time (mins)", "pSTAT3Y705/STAT3")
+  
+#Use ggplot2
+  Female_T3_1Ms_Bcell_IL6_TC %>% ggplot(aes(x = `Time (mins)`, y = `pSTAT3Y705/STAT3`, color = Genotype)) +
+    geom_point() +
+    #facet_wrap(vars(Genotype)) +
+    labs(title = "Female", x = "Time (mins)", y = "pSTAT3Y705/STAT3") + 
+    theme(axis.title = (element_text(size = 13, face = 'bold')))
+  
+#make averages for each 
+  WT_Females <- ALL_T3_1Ms_Bcell_IL6_TC %>% filter(Genotype=='WT', Sex=="Female") %>% 
+    select(`Time (mins)`, `pSTAT3Y705/STAT3`, `Mouse Number`) %>% 
+    pivot_wider(id_cols = "Time (mins)", names_from = "Mouse Number", values_from = "pSTAT3Y705/STAT3") %>% 
+    mean()
+  
+    
+  
